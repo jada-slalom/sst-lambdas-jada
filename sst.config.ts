85f8464bea931bbb60d9ff2717cfae1f2b34db89
anyunSlalom <anyun.li@slalom.com>
2024-04-17 12:42:05 -0700

    cleanup

--- sst.config.ts
+++ sst.config.ts
@@ -1,17 +1,18 @@
 import { SSTConfig } from "sst";
+import pkg from './package.json';
 import { APIStack } from "./stacks/APIStack";
 import { LambdaStack } from "./stacks/LambdaStack";
 
 export default {
+  
   config(_input) {
     return {
-      name: "payment",
+      name: pkg.name.split(/[/]+/).pop(),
       region: "us-west-1",
     };
   },
   stacks(app) {
     app.stack(LambdaStack, { stackName: `${app.name}-lambdas-${app.stage}` });
     app.stack((stack)=> APIStack(stack, 'packages/openapi/swagger.yaml'), { stackName: `${app.name}-api-${app.stage}` });
-    
   }
 } satisfies SSTConfig;
