ca31c239b017f406b1e672de0e7fe831ebd7673a
Jada Berenguer <jada.berenguer@slalom.com>
2024-04-18 07:52:54 -0700

    Change name to trigger deployment

--- stacks/LambdaStack.ts
+++ stacks/LambdaStack.ts
@@ -10,7 +10,7 @@ export function LambdaStack({
   app,
   stack,
 }: StackContext): Record<string, FunctionProps> {
-  const nestjslayer = new lambda.LayerVersion(stack, "nestjsLayerJadaTest", {
+  const nestjslayer = new lambda.LayerVersion(stack, "nestjsLayerJada", {
     code: lambda.Code.fromAsset("layers/nestjs"),
   });
 
